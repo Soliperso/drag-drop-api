316e8ab797cf87937decabc02535bb8001655b8d
Soliperso <chah762002@yahoo.fr>
2022-03-08 10:03:41 -0800

    "Insert List items into the DOM"

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ const richestPeople = [
   'Larry Ellison',
 ];
 
+const listItems = [];
+let dragStartIndex;
+createList();
+
+function createList() {
+  [...richestPeople].forEach((person, index) => {
+    const listItem = document.createElement('li');
+    listItem.setAttribute('data-index', index);
+    listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+            <p class="person-name">${person}</p>
+            <i class="fas fa-grip-lines"></i>
+        </div>
+        `;
+    listItems.push(listItem);
+    draggable_list.appendChild(listItem);
+  });
+}
