7d0cc8820645c88d203ecc3e7ed1add6e32486ad
Soliperso <chah762002@yahoo.fr>
2022-03-08 09:59:20 -0800

    "Setup basic structure"

---
+++ script.js
@@ -0,0 +1,17 @@
+let draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richestPeople = [
+  'Jeff Bezos',
+  'Bill Gates',
+  'Warren Buffett',
+  'Bernard Arnault',
+  'Carlos Slim Helu',
+  'Amancio Ortega',
+  'Mark Zuckerberg',
+  'Michael Bloomberg',
+  'Larry Page',
+  'Sergey Brin',
+  'Larry Ellison',
+];
+
